88cc7d41304a7668e47656efb8b78f9801579dbd
Cory <cory@Corys-MacBook-Pro.local>
2026-07-27 09:58:42 -0700

    Move circuit plan to homepage hero

--- app/concept/ConceptExperience.tsx
+++ app/concept/ConceptExperience.tsx
@@ -542,8 +542,7 @@ export default function ConceptExperience() {
         </aside>
       </section>
 
-      <div className={styles.mainContent}>
-        <section className={styles.problemSection} id="concept-problems">
+      <section className={styles.problemSection} id="concept-problems">
         <div
           className={styles.sectionHeading}
           id="concept-problem-heading"
@@ -658,13 +657,13 @@ export default function ConceptExperience() {
             </p>
           </aside>
         </div>
-        </section>
+      </section>
 
-        <WhyGoldenStateVisionsSection />
+      <WhyGoldenStateVisionsSection />
 
-        <TechnologyPartnersSection />
+      <TechnologyPartnersSection />
 
-        <section className={styles.howSection} id="how-we-work">
+      <section className={styles.howSection} id="how-we-work">
         <div className={styles.whyIntro}>
           <p className={styles.eyebrow}>How We Work</p>
           <h2>A clear path from first conversation to long-term support.</h2>
@@ -700,8 +699,7 @@ export default function ConceptExperience() {
             </p>
           </article>
         </div>
-        </section>
-      </div>
+      </section>
 
       <SiteFooter />
     </main>
